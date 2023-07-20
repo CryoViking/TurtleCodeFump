073192c3330c32be7296c138dac7c7f78f73cb2c
cryosis <nathanvdvelde@gmail.com>
2023-07-20 23:22:43 +0800

    Adding download to make it easier

--- download.lua
+++ download.lua
@@ -1,10 +1,10 @@
 args = { ... }
--- os.loadAPI("wgetAPI")
+os.loadAPI("wgetAPI")
 
 print("This is a test")
 local filename = args[1]
 local base_url = "https://raw.githubusercontent.com/CryoViking/TurtleCodeFump/master/"
-print(base_url + filename)
--- wget.downloadFile(base_url+)
+print(base_url .. filename)
+wget.downloadFile(base_url .. filename, filename)
 --
-wget()
+--wget()
