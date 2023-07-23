d2389dcdd69b27225a1da26599ff33a07a0489de
cryosis <nathanvdvelde@gmail.com>
2023-07-23 16:32:36 +0800

    Not the ender chest

--- roam.lua
+++ roam.lua
@@ -331,7 +331,7 @@ end
 
 -- function to place an ender chest from the inventory and interact with it
 local function placeAndInteractWithEnderChest()
-	local chestSlot = findItem(Blocks.SPRUCE_CHEST)
+	local chestSlot = findItem(Blocks.ENDER_CHEST)
 	if chestSlot then
 		turnAround()
 		if dig() == true then
